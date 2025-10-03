70ddcbcba2104ade30b25beb30d60eee833e23ea
superior090 <chibuihegab@gmail.com>
2025-10-03 16:19:47 +0100

    yooo

--- users/management.py
+++ users/management.py
@@ -45,7 +45,7 @@ def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depend
 
 
 @router.get("/users/", response_model=List[UserOut])
-def list_users(db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+def list_users(db: Session = Depends(get_db)): #current_user: User = Depends(get_current_user)):
     return db.query(User).all()
 
 
